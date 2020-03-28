6e7c61968844da44ee2585223b9dffa794ca52c2
Alex <alex@dzvo.org>
2020-03-28 07:50:12 -0400

    Add javadoc, change signature of create/update/delete and read to comply with http codes

--- src/main/java/org/dzvo/bilanzierungsgebiete/service/GebietController.java
+++ src/main/java/org/dzvo/bilanzierungsgebiete/service/GebietController.java
@@ -3,11 +3,12 @@ package org.dzvo.bilanzierungsgebiete.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Example;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//FIXME Add javadoc/comments
 //FIXME improve REST level to 3
 @RestController
 public class GebietController {
@@ -21,6 +22,15 @@ public class GebietController {
     //SEARCH
     //FIXME add unit test
     //FIXME add more parameters
+
+    /**
+     * Is used to search for a certain "Bilanzierungsgebiet" by supplying the corresponding parameters.
+     * If none are passed will return all "Bilanzierungsgebiet" currently in database.
+     *
+     * @param stromnetzbetreiber
+     * @param bilanzierungsgebietEIC
+     * @return Results of search
+     */
     @RequestMapping(value = "/gebiet", method = RequestMethod.GET)
     public List<Gebiet> gebietListing(
             @RequestParam(value = "stromnetzbetreiber", required = false) String stromnetzbetreiber,
@@ -38,8 +48,16 @@ public class GebietController {
     //CREATE
     //FIXME add unit test
     //FIXME add more parameters
+
+    /**
+     * Inserts a new entry, parameters not supplied will be set to null. This method won't prevent duplicates.
+     *
+     * @param stromnetzbetreiber
+     * @param bilanzierungsgebietEIC
+     * @return The newly created entry
+     */
     @RequestMapping(value = "/gebiet", method = RequestMethod.PUT)
-    public Boolean gebietInsert(
+    public Gebiet gebietInsert(
             @RequestParam(value = "stromnetzbetreiber", required = false) String stromnetzbetreiber,
             @RequestParam(value = "bilanzierungsgebiet-eic", required = false) String bilanzierungsgebietEIC
     ) {
@@ -50,15 +68,24 @@ public class GebietController {
 
         repo.save(q);
 
-        //FIXME check for duplicates
-        return true;
+        //FIXME check for duplicates?
+        return q;
     }
 
     //UPDATE
     //FIXME add unit test
     //FIXME add more parameters
+
+    /**
+     * Updates records values. Not supplied parameters are left as-is.
+     *
+     * @param id                     The id that identifies the record to update
+     * @param stromnetzbetreiber
+     * @param bilanzierungsgebietEIC
+     * @return HTTP 200 and the new record itself if record to be updated was found, HTTP 404 if not
+     */
     @RequestMapping(value = "/gebiet/{id}", method = RequestMethod.POST)
-    public Boolean gebietUpdateId(
+    public ResponseEntity gebietUpdateId(
             @PathVariable(value = "id") String id,
             @RequestParam(value = "stromnetzbetreiber", required = false) String stromnetzbetreiber,
             @RequestParam(value = "bilanzierungsgebiet-eic", required = false) String bilanzierungsgebietEIC
@@ -69,26 +96,43 @@ public class GebietController {
             if (bilanzierungsgebietEIC != null) e.setBilanzierungsgebietEIC(bilanzierungsgebietEIC);
 
             repo.save(e);
-            return true;
+            return ResponseEntity.ok(e);
         }
-        return false;
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("");
     }
 
     //DELETE
     //FIXME add unit test
+
+    /**
+     * Delete a record by id.
+     *
+     * @param id
+     * @return HTTP 200 if record was successfully deleted, 404 if not found.
+     */
     @RequestMapping(value = "/gebiet/{id}", method = RequestMethod.DELETE)
-    public Boolean gebietDeleteId(@PathVariable(value = "id") String id) {
-        if (repo.existsById(Long.valueOf(id))) {
-            repo.deleteById(Long.valueOf(id));
-            return true;
+    public ResponseEntity gebietDeleteId(@PathVariable(value = "id") int id) {
+        if (repo.existsById((long) id)) {
+            repo.deleteById((long) id);
+            return ResponseEntity.ok("");
         }
-        return false;
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("");
     }
 
     //READ
     //FIXME add unit test
+
+    /**
+     * Return a record by Id
+     *
+     * @param id
+     * @return The specified record or HTTP 404 if not found
+     */
     @RequestMapping(value = "/gebiet/{id}", method = RequestMethod.GET)
-    public Gebiet gebietGetById(@PathVariable(value = "id") String id) {
-        return repo.findById(Long.valueOf(id)).orElseThrow();
+    public ResponseEntity gebietGetById(@PathVariable(value = "id") int id) {
+        if (repo.existsById((long) id)) {
+            return ResponseEntity.ok(repo.findById(Long.valueOf(id)));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("");
     }
 }
