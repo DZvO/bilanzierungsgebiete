c64530080a29e4b4b871c5ac9ba765c2434c35ea
Alex <alex@dzvo.org>
2020-03-28 14:35:11 -0400

    Removes unnecessary methods in GebietRepository interface

--- src/main/java/org/dzvo/bilanzierungsgebiete/service/GebietRepository.java
+++ src/main/java/org/dzvo/bilanzierungsgebiete/service/GebietRepository.java
@@ -2,13 +2,6 @@ package org.dzvo.bilanzierungsgebiete.service;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-
 public interface GebietRepository extends JpaRepository<Gebiet, Long> {
     Gebiet findById(long id);
-
-    List<Gebiet> findByStromnetzbetreiber(String stromnetzbetreiber);
-
-    List<Gebiet> findByBilanzierungsgebietEIC(String bilanzierungsgebietEIC);
 }
